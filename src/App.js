a210ef1326a352e154f84bf53a6189bc1a1371a6
YoungSTAR <chinusohlot@gmail.com>
2022-09-20 17:09:32 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import View from './components/View';
+// import Input from './components/Input';
+// import { useEffect, useState } from 'react';
+
+function App() {
+ 
+  
+  return (
+    <div className="App">
+      <View/>
+    </div>
+  );
+}
+
+export default App;
